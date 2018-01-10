eb0e8f2cb704c2ee88a621cbea83e510245abfd1
Mohit Reddy <mohitreddy1996@gmail.com>
2018-01-10 21:54:26 +0530

    Refactoring and Q8 added.

--- Assignment1/Q5/pi_normal.cc
+++ Assignment1/Q5/pi_normal.cc
@@ -1,7 +1,7 @@
 /*
- * PI Calculation using Worksharing and Reduction
+ * Calculating value of PI parallely
  * Run the program as follows
- * (Compilation) g++ -fopenmp PI_program.cpp 
+ * (Compilation) g++ -fopenmp pi_normal.cc 
  * (Execution) ./a.out THREAD_COUNT NO_OF_STEPS
  * Arguments
  * 1) THREAD_COUNT. (Optional Parameter) Number of threads to be created. By default uses 
@@ -24,8 +24,9 @@
 
 namespace {
 
-double pi_worksharing_and_reduction_parallel(int no_of_threads, int no_of_steps) {
+double pi_parallel(int no_of_threads, int no_of_steps, int flag) {
 
+	
 	//Setting number of threads based on the passed parameter
 	if(no_of_threads > 0)
 		omp_set_num_threads(no_of_threads);
@@ -41,32 +42,55 @@ double pi_worksharing_and_reduction_parallel(int no_of_threads, int no_of_steps)
 		step_count = no_of_steps;
 	else
 		step_count = NO_OF_STEPS;
-	double step, sum = 0.0, pi, x;
+	double step, pi = 0.0;
 
 	//Calculating the step size, we are integrating from 0 to 1
 	step = 1.0/(double)step_count;
 
 	// Starting timer
 	double begin = omp_get_wtime();
-
+	double sum = 0.0, x;
+	int i;
 	//Parallel step
-	#pragma omp parallel for private(x) reduction(+:sum)
-		for(int i=0;i<step_count;i++)
-		{
+	#pragma omp parallel shared(pi,flag) private(x,sum,i)
+	{
+		
+		int id;
+		id = omp_get_thread_num();
+		int n_threads;
+		n_threads = omp_get_num_threads();
+
+		#pragma omp for
+		for(i = 0; i< step_count; i++) {
 			x = (i + 0.5)*step;
 			sum = sum + 4.0/(1.0 + x*x);	
 		}
 
-	//Area calculation
-	pi = sum*step;
+		//Sum is private to each thread, multiplying sum with step to reduce the no of operations in critical section
+		sum = sum * step;
+		
+		if(flag == 0) {
+			//Only one operation in critical section
+			#pragma omp critical
+				pi += sum;
+		}
+		else {
+			//Only one operation in critical section
+			#pragma omp atomic
+				pi += sum;
 
+		}
+
+	}
+
+	if(flag == 0)
+		printf("PI value is %lf, Calculated using %d number of threads and %d number of steps\n",pi,no_of_threads,step_count);
 	// Stopping timer
 	double end = omp_get_wtime();
 
-	printf("PI value is %lf, Calculated using %d number of threads and %d number of steps\n",pi,no_of_threads,step_count);
-
 	return end - begin;
 }
+
 double pi_serial( int no_of_steps) {
 	
 	int step_count;
@@ -93,27 +117,32 @@ double pi_serial( int no_of_steps) {
 
 int main(int argc, char* argv[]) {
 
-	double time_taken_parallel;
-	double time_taken_serial;	
+	double time_taken_critical;
+	double time_taken_atomic;	
+	double time_serial;
 	if(argc == 1) {
-		time_taken_parallel = pi_worksharing_and_reduction_parallel(0,0);
-		time_taken_serial = pi_serial(0);
+		time_taken_critical = pi_parallel(0,0,0);
+		time_taken_atomic = pi_parallel(0,0,1);
+		time_serial = pi_serial(0);
 	}
 	else if(argc == 2) {
-		time_taken_parallel = pi_worksharing_and_reduction_parallel(atoi(argv[1]),0);
-		time_taken_serial = pi_serial(0);
+		time_taken_critical = pi_parallel(atoi(argv[1]),0,0);
+		time_taken_atomic = pi_parallel(atoi(argv[1]),0,1);
+		time_serial = pi_serial(0);
 	}
-	
 	else if(argc == 3) {
-		time_taken_parallel = pi_worksharing_and_reduction_parallel(atoi(argv[1]),atoi(argv[2]));
-		time_taken_serial = pi_serial(atoi(argv[2]));
-	}
+		time_taken_critical = pi_parallel(atoi(argv[1]),atoi(argv[2]),0);
+		time_taken_atomic = pi_parallel(atoi(argv[1]),atoi(argv[2]),1);
+		time_serial = pi_serial(atoi(argv[2]));
 
+	}
 	else {
 		printf("ERROR: Provide Correct Number of arguments\n");
 		exit(0);
 	}
 
-	printf("Time taken for parallel version is : %lf\n", time_taken_parallel);
-	printf("Time taken for Serial version is : %lf\n", time_taken_serial);
+	printf("Time taken for parallel version is using critical construct is : %lf Seconds\n", time_taken_critical);
+	printf("Time taken for parallel version is using atomic construct is : %lf Seconds\n", time_taken_atomic);
+	printf("Time taken for Serial execution is : %lf Seconds\n", time_serial);
+	
 }
---
+++ Assignment1/Q8/producer_consumer.cc
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <iostream>
+#include <time.h>
+
+#define N 1000
+#define MAX 100
+#define MIN 10
+
+namespace {
+
+double get_random() {
+    double before = rand() % (int) MAX + (int) MIN;
+    double after = (double) rand() / RAND_MAX;
+    double result = before + after;
+    return result;
+}
+
+double get_sum(double arr[], int size) {
+    double sum = 0.0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+void reset(double arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = get_random();
+    }
+    return;
+}
+
+double get_linear_execution_time(double arr[], int size) {
+    double start_time = omp_get_wtime();
+    reset(arr, size); // producer.
+    double sum = get_sum(arr, size); // consumer.
+    double end_time = omp_get_wtime();
+    return end_time - start_time;
+}
+
+double get_parallel_execution_time(double arr[], int size) {
+    int flag = 0;
+    double sum = 0.0;
+    double start_time = omp_get_wtime();
+    #pragma omp sections
+    {
+        #pragma omp section
+        {
+            // producer.
+            #pragma omp parallel for shared(arr)
+            for (int i = 0; i < size; i++) {
+                arr[i] = get_random();
+            }
+            #pragma atomic write
+                flag = 1;
+        }
+        #pragma omp section
+        {
+            // consumer.
+            int flag_read = 0;
+            while (1) {
+                #pragma omp flush(flag)
+                #pragma omp atomic read
+                    flag_read = flag;
+                if (flag_read == 1)
+                    break;
+            }
+            #pragma omp parallel for shared(sum, arr)
+            for (int i = 0; i < size; i++) {
+                #pragma omp atomic
+                sum += arr[i];
+            }
+        }
+    }
+    double end_time = omp_get_wtime();
+    // works fine! tested!
+    // printf("Parallel sum: %lf. Serial sum: %lf.\n", sum, get_sum(arr, size));
+    return end_time - start_time;
+}
+
+}
+
+int main(int argc, char *argv[]) {
+    int size = N;
+    srand(time(NULL));
+    if (argc > 1) {
+        if (std::string(argv[1]) == "-n") {
+            size = atoi(argv[2]);
+            if (size <= 0) {
+                printf("Size of the input cannot be wrong.\n");
+                exit(1);
+            }
+        } else {
+            printf("Wrong command line argument.\n");
+            exit(1);
+        }
+    }
+    double arr[size];
+    double linear_time = get_linear_execution_time(arr, size);
+    printf("Linear producer-consumer execution time: %lf seconds.\n", linear_time);
+    double parallel_time = get_parallel_execution_time(arr, size);
+    printf("Parallel producer-consumer execution time: %lf seconds.\n", parallel_time);
+    return 0;
+}
