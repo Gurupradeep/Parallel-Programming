8a00efae8eff0f316dbd93d2d4bbe1e1ab9a0b90
Mohit Reddy <mohitreddy1996@gmail.com>
2018-01-22 06:19:31 +0530

    Q9 added.

---
+++ Assignment2/Q9/index_data_type.cc
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define N 5
+
+int main () {
+    int displacement[N], block_lengths[N];
+    int matrix[N*N];
+    MPI_Datatype matrix_indexed_datatype;
+    MPI_Status status;
+    MPI_Init(NULL, NULL);
+    int world_size, world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    for (int i = 0; i < N; i++) {
+        block_lengths[i] = N - i;
+        displacement[i] = i * N + i; // 1 D array.
+    }
+
+    MPI_Type_indexed(N, block_lengths, displacement, MPI_INT, &matrix_indexed_datatype);
+    MPI_Type_commit(&matrix_indexed_datatype);
+
+    if (world_rank == 0) {
+        for (int i = 0; i < N * N; i++) {
+            matrix[i] = i + 1; // matrix[0][0] = 0 might confuse output.
+        }
+        MPI_Send(matrix, 1, matrix_indexed_datatype, 1, 0, MPI_COMM_WORLD);
+    } else if (world_rank == 1) {
+        for (int i = 0; i < N * N; i++) {
+            matrix[i] = 0;
+        }
+        MPI_Recv(matrix, 1, matrix_indexed_datatype, 0, 0, MPI_COMM_WORLD, &status);
+        int elements = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                printf("%d ", matrix[elements]);
+                elements++;
+            }
+            printf("\n");
+        }
+    } else {
+    
+    }
+    MPI_Finalize();
+    return 0;
+}
